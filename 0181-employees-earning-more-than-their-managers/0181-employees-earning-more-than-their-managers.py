1e034e7d30635aa2afa593ecb7687973f3c569be
Đặng Tân Đạt <dangtandat.tech@gmail.com>
2026-04-11 22:18:43 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
@@ -1,12 +1,7 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    result = employee.merge(
-        employee,
-        left_on = 'managerId',
-        right_on = 'id',
-        suffixes = ('_em', '_mg')
-    )
-    result = result[result['salary_em'] > result['salary_mg']]
-    result = result[['name_em']].rename(columns = {'name_em': 'Employee'})
+    result = pd.merge(employee, employee, left_on = 'managerId', right_on = 'id', how='left')
+    result = result[result['salary_x'] > result['salary_y']]
+    result = result[['name_x']].rename(columns = {'name_x': 'Employee'})
     return result
