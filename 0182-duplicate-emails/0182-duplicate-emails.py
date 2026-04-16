24164b2dd03863d587db7b4d0708da0cf3bbf081
Đặng Tân Đạt <dangtandat.tech@gmail.com>
2026-04-16 14:39:47 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    result = person[person.duplicated(subset=['email'], keep=False)]
-    result = result['email'].drop_duplicates().to_frame()
-    return result[['email']].rename(columns={'email': 'Email'})
+    res = person[person['email'].duplicated()]['email'].unique()
+    return pd.DataFrame({'Email': res})
